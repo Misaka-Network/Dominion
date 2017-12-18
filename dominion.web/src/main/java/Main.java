42ba8b266ae97aed17457400804bd53dc7ab3b64
hyzhou.zhy <hyzhou.zhy@alibaba-inc.com>
2017-12-18 15:47:32 +0800

    fix the groovy lost.

---
+++ dominion.web/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hyzhou.zhy. 2017/12/17.
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
